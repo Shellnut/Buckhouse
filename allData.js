f085aa16d64fa1dcd7e12bca1090097b8bdca325
Shellnut <shellnut@gmail.com>
2021-09-15 23:18:45 -0600

    adding 9/16 video

--- allData.js
+++ allData.js
@@ -12,7 +12,7 @@
 //       }
 //   }  
 // }
-var lastUpdated = '09/15/2021';
+var lastUpdated = '09/16/2021';
 var allData = [
   {
     "resortName": "Mohawk Mountain Ski Area",
@@ -1901,6 +1901,7 @@ var allData = [
     "region": "Mid-Atlantic",
     "city": "East Rutherford (indoor)",
     "videos": [
+      "pRkpId3qqJw",
       "5ZDB0QuoFxc",
       "1b0xhSsTEOw",
       "-vhU47d1AGk",

a52f439efb5e9e1b87781faf460ddbded7935262
shellnut <shellnut@gmail.com>
2021-11-23 06:22:37 -0700

    adding 11/22 video

--- allData.js
+++ allData.js
@@ -12,7 +12,7 @@
 //       }
 //   }  
 // }
-var lastUpdated = '11/21/2021';
+var lastUpdated = '11/22/2021';
 var allData = [
   {
     "resortName": "Mohawk Mountain Ski Area",
@@ -6759,6 +6759,7 @@ var allData = [
     "region": "Rocky Mountains",
     "city": "Copper Mountain",
     "videos": [
+      "p2a51k84avI",
       "1eQ3c-Sz1uE",
       "4AU50LCYEPw",
       "kM2VhNJuRs4",

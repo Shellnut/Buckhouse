96600056df4a437574d8c385c75b54f4836c438d
Shellnut <shellnut@gmail.com>
2024-09-13 22:26:14 +0000

    adding links to treadmills

--- resortData/carpetTreadmill/carpetTreadmillData.js
+++ resortData/carpetTreadmill/carpetTreadmillData.js
@@ -22,7 +22,7 @@ var carpetTreadmill = [
             "note": ""
         },
         "skiPass": [],
-        "mapLink": ""
+        "mapLink": "https://snobahn.com/wp-content/uploads/2024/02/IMG_5826-890x664.jpg"
     },
     {
         "resortName": "Snobahn (Centennial)",
@@ -44,7 +44,7 @@ var carpetTreadmill = [
             "note": ""
         },
         "skiPass": [],
-        "mapLink": ""
+        "mapLink": "https://snobahn.com/wp-content/uploads/2022/11/smSnobahn-10.jpg"
     },
     {
         "resortName": "Infinity Slopes",
@@ -68,6 +68,50 @@ var carpetTreadmill = [
             "note": ""
         },
         "skiPass": [],
-        "mapLink": ""
+        "mapLink": "https://images.squarespace-cdn.com/content/v1/65667199528056409c6e760d/fe38e26c-a17e-4461-aae7-67cae7ac2a20/IMG_0956.jpg"
+    },
+    {
+        "resortName": "Ski Easy",
+        "state": "",
+        "region": "",
+        "country": "United Kingdom",
+        "city": "London",
+        "videos": [],
+        "position": {
+            "lat": 51.47448804991069,
+            "lng": -0.2635842206379553
+        },
+        "officialSkiResort": true,
+        "id": "",
+        "website": "https://www.skieasy.net/",
+        "resortReview": {
+            "score": "",
+            "link": "",
+            "note": ""
+        },
+        "skiPass": [],
+        "mapLink": "https://images.squarespace-cdn.com/content/v1/5a007d0fa803bb45eefce1fa/1510068130481-3DC7RFIY62SS090LKOQ6/DM-SkiEasy-_DSC1048_preview.jpeg"
+    },
+    {
+        "resortName": "Alpine Factory",
+        "state": "Minnesota",
+        "region": "Midwest",
+        "country": "United States",
+        "city": "London",
+        "videos": [],
+        "position": {
+            "lat": 45.057918152269224,
+            "lng": -93.15260777246692
+        },
+        "officialSkiResort": true,
+        "id": "",
+        "website": "https://thealpinefactory.com/",
+        "resortReview": {
+            "score": "",
+            "link": "",
+            "note": ""
+        },
+        "skiPass": [],
+        "mapLink": "https://thealpinefactory.com/user/themes/alpinefactory/images/contacthero.jpg"
     },
 ];
